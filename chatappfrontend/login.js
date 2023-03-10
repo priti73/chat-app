64b6ba03e4f51444203703c280e1f40d9872c237
priti73 <pritikumarinits@gmail.com>
2023-03-10 21:34:40 +0530

    login.js

---
+++ chatappfrontend/login.js
@@ -0,0 +1,34 @@
+const email=document.querySelector('#email');
+const password=document.querySelector('#password');
+
+ async function login(e){
+   try{
+        e.preventDefault();
+        let logindetails={
+            email:email.value,
+            password:password.value
+          }
+      
+    console.log(logindetails);
+    const response= await axios.post("http://localhost:3000/users/login",
+    logindetails)
+    if(response.status==201){
+        console.log('ok');
+       
+    }
+    
+        }catch(err){
+         console.log(err);
+         document.body.innerHTML=`<div style="color:red;">${err.message} <div>`
+    }
+}
+
+document.getElementById('reset-button').onclick=async function(e){
+    try{
+        window.location.href="./resetform.html"
+    }
+    catch(err){
+        console.log(err);
+        document.body.innerHTML=`<div style="color:red;">${err.message} <div>`
+    }
+}
